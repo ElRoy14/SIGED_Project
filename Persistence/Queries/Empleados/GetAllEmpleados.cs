bcd1caabf717ff3a730c9aefc4d47d8b0e435140
ElRoy14 <roylinrodriguez5@gmail.com>
2024-05-29 15:30:48 -0400

    Upload: Subiendo Estructura del proyecto a al repositorio remoto

---
+++ Persistence/Queries/Empleados/GetAllEmpleados.cs
@@ -0,0 +1,44 @@
+﻿using Persistence.DBConnections;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Queries.Empleados
+{
+    public class GetAllEmpleados
+    {
+        private SqlConnection _conn = null;
+
+        public GetAllEmpleados()
+        {
+            _conn = DBConnection.Connect();
+        }
+
+        public void ReadEmpleados()
+        {
+            using (_conn)
+            {
+                SqlCommand comd = new SqlCommand("SELECT * FROM dbo.Empleados", _conn);
+
+                SqlDataReader rd = comd.ExecuteReader();
+
+                try
+                {
+                    while (rd.Read())
+                    {
+                        Console.WriteLine(String.Format("{0}, {1}", rd[0], rd[1]));
+                    }
+                }
+                finally
+                {
+                    rd.Close();
+                }
+            }
+        }
+
+
+    }
+}
