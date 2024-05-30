c9fd173095970643171747ae5975db62649722b8
ElRoy14 <roylinrodriguez5@gmail.com>
2024-05-30 07:13:56 -0400

    Feature/Fix: Se agregó la entidad empleados, los DTO de empleados y los queries para las operaciones de acceso a datos de los empledos

---
+++ Application/DTOs/Empleados/SaveEmpleadoDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Empleados
+{
+    public class SaveEmpleadoDTO
+    {
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Telefono { get; set; }
+
+        public string? Cargo { get; set; }
+
+        public string? Departamento { get; set; }
+
+        public string? Superior { get; set; }
+
+        public string? Estatus { get; set; }
+    }
+}
