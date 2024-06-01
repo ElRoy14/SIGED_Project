8e1ae7a36cc35c822e871259870350a45d914a94
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:34:02 +0200

    genericRepository added

---
+++ Siged.Infrastructure/Repositorys/GenericRepository.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Siged.Domain;
+using Siged.Domain.Interfaces;
+using System.Linq.Expressions;
+
+namespace Siged.Infrastructure.Repositorys
+{
+    public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
+    {
+        private readonly SigedContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GenericRepository(SigedContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TModel> GetEverythingAsync(Expression<Func<TModel, bool>> filter)
+        {
+            try
+            {
+                TModel model = await _dbContext.Set<TModel>().FirstOrDefaultAsync(filter);
+                return model;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<TModel> CreateAsync(TModel model)
+        {
+            try
+            {
+                _dbContext.Set<TModel>().Add(model);
+                await _dbContext.SaveChangesAsync();
+                return model;
+            }
+            catch
+            {
+                throw;
+
+            }
+
+        }
+
+        public async Task<bool> UpdateAsync(TModel model)
+        {
+            try
+            {
+                _dbContext.Set<TModel>().Update(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<bool> DeleteAsync(TModel model)
+        {
+            try
+            {
+                _dbContext.Set<TModel>().Remove(model);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+        public async Task<IQueryable<TModel>> VerifyDataExistenceAsync(Expression<Func<TModel, bool>> filter = null)
+        {
+            try
+            {
+                IQueryable<TModel> ModelQuery = filter == null ? _dbContext.Set<TModel>() : _dbContext.Set<TModel>().Where(filter);
+                return ModelQuery;
+            }
+            catch
+            {
+                throw;
+            }
+
+        }
+
+    }
+
+}
