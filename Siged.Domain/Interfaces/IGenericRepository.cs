40d5eb19a1aa63095c7424c5e74a7a4519cf3e2e
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:29:47 +0200

    interfaces added

---
+++ Siged.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+namespace Siged.Domain.Interfaces
+{
+    public interface IGenericRepository<TModel> where TModel : class
+    {
+        Task<TModel> GetEverythingAsync(Expression<Func<TModel, bool>> filter);
+        Task<TModel> CreateAsync(TModel model);
+        Task<bool> UpdateAsync(TModel model);
+        Task<bool> DeleteAsync(TModel model);
+        Task<IQueryable<TModel>> VerifyDataExistenceAsync(Expression<Func<TModel, bool>> filter = null);
+
+    }
+
+}
