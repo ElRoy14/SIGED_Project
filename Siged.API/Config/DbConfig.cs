bd0d21ac349dfcde7b95269f316f2869c7d52441
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:47:29 +0200

    dbconfig added

---
+++ Siged.API/Config/DbConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Siged.Domain;
+
+namespace Siged.API.Config
+{
+    public static class DbConfig
+    {
+        public static IServiceCollection ConfigDbConnection(this IServiceCollection service, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection")!;
+            service.AddDbContext<SigedContext>(options => options.UseSqlServer(connectionString));
+
+            return service;
+        }
+
+    }
+
+}
