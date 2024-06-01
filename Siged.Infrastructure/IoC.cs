6998693f7458a81587ffc3c6e01166d350fd4848
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:34:40 +0200

    IoC added

---
+++ Siged.Infrastructure/IoC.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Siged.Domain.Interfaces;
+using Siged.Infrastructure.Repositorys;
+
+namespace Siged.Infrastructure
+{
+    public static class IoC
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection service)
+        {
+            return service.
+                AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        }
+
+    }
+
+}
