fd7e38ecf27241f41cb5c2e33a5aed1d89374e30
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:49:23 +0200

    Add program configuration for AutoMapper, database connection, and JWT authentication

--- Siged.API/Program.cs
+++ Siged.API/Program.cs
@@ -1,7 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Siged.API.Config;
+using Siged.Application;
+using Siged.Infrastructure;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+builder.Services
+    .AddApplication()
+    .AddRepositories();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.ConfigDbConnection(builder.Configuration);
+
+var key = builder.Configuration.GetValue<string>("JwtSettings:key");
+var keyBytes = Encoding.ASCII.GetBytes(key);
+
+builder.Services.AddAuthentication(config =>
+{
+    config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(config =>
+{
+    config.RequireHttpsMetadata = false;
+    config.SaveToken = true;
+    config.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero,
+    };
+});
 
 var app = builder.Build();
 
