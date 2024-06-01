26c1060e5d917a6b9597d434527e6e1c55e06282
LuisLiriano03 <llirianomendez@gmail.com>
2024-06-01 06:44:13 +0200

    Add roles-related Dtos, AutoMapper, Exceptions, Interfaces, Services

---
+++ Siged.Application/Roles/Exceptions/GetRolFailedException.cs
@@ -0,0 +1,13 @@
+﻿namespace Siged.Application.Roles.Exceptions
+{
+    public class GetRolFailedException : Exception
+    {
+        public override string Message { get; }
+
+        public GetRolFailedException() : base()
+        {
+            Message = "No role found";
+        }
+
+    }
+}
