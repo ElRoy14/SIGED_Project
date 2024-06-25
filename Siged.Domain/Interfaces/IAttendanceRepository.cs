a134d6ca717dd175a7022876239da24285b3dba6
JuniorCalvo <juniorcalvo0410@gmail.com>
2024-06-25 04:44:59 +0200

    added new repository

---
+++ Siged.Domain/Interfaces/IAttendanceRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siged.Domain.Interfaces
+{
+    public interface IAttendanceRepository<TModel> where TModel : class
+    {
+        Task<IQueryable<TModel>> GetAllAttendanceAsync(Expression<Func<TModel, bool>> filter = null);
+        Task<TModel> CheckIn(TModel model);
+        Task<TModel> CheckOut(TModel model);
+    }
+}
