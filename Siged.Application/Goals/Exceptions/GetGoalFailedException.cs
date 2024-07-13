8b86ba9a875da3fedf66461f51173d9a614a4a73
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-14 01:39:07 +0200

    added mappers, dtos, exceptions, interface, service about goals

---
+++ Siged.Application/Goals/Exceptions/GetGoalFailedException.cs
@@ -0,0 +1,14 @@
+﻿namespace Siged.Application.Goals.Exceptions
+{
+    public class GetGoalFailedException : Exception
+    {
+        public override string Message { get; }
+
+        public GetGoalFailedException() : base()
+        {
+            Message = "No gols found";
+        }
+
+    }
+
+}
