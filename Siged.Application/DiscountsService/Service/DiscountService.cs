5332e618e360004dce0342a32dd9fbdaf45f1883
LuisLiriano03 <llirianomendez@hotmail.com>
2024-07-23 04:27:00 +0200

    Implemented AutoMapper profiles, DTOs, exceptions, interfaces, and services on discountsService

---
+++ Siged.Application/DiscountsService/Service/DiscountService.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Siged.Application.DiscountsService.Interface;
+using Siged.Domain.Interfaces;
+using Siged.Domain;
+using Siged.Application.DiscountsService.DTOs;
+using Siged.Application.DiscountsService.Exceptions;
+
+namespace Siged.Application.DiscountsService.Service
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly IGenericRepository<Discount> _discountRepository;
+        private readonly IMapper _mapper;
+
+        public DiscountService(IGenericRepository<Discount> discountRepository, IMapper mapper)
+        {
+            _discountRepository = discountRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<GetDiscount>> GetDiscountsAsync()
+        {
+            try
+            {
+                var payment = await _discountRepository.VerifyDataExistenceAsync();
+                return _mapper.Map<List<GetDiscount>>(payment.ToList());
+            }
+            catch
+            {
+                throw new GetDiscountFailedException();
+            }
+
+        }
+
+    }
+
+}
